8791cf8d52c57583472ea0338856e995c5ec979d
denizytm <denizyetim@hotmail.com>
2025-02-24 17:48:58 +0300

    migrations added

--- backend/Models/Project.cs
+++ backend/Models/Project.cs
@@ -7,12 +7,12 @@ namespace backend.Models
 {
     public class Project
     {
-          public int Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; } = String.Empty;
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string Status { get; set; } = String.Empty; // Örneğin: "In Progress", "Completed"
-        
+
         // İlişkiler
         public ICollection<Task> Tasks { get; set; }   // Projede birden fazla görev olabilir
     }
