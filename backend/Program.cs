3c3203df8a6ff7e76cfcf5fb9b19d70e25bd20d6
denizytm <denizyetim@hotmail.com>
2025-03-02 12:02:46 +0300

    chat model added

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,21 +3,39 @@ using backend.Extensions;
 using backend.Interfaces;
 using backend.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using backend.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.ConfigureSignalR();
 builder.Services.AddControllers();
 builder.Services.ConfigureCors();
-builder.Services.AddScoped<IUserRepository,UserRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddHttpClient();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => {
+#pragma warning disable SKEXP0070
+
+builder.Services.AddKernel()
+    .AddGoogleAIGeminiChatCompletion("gemini-1.5-pro","AIzaSyD-40W--DxNGGUGYcBg6qjYNEkuhkKKwKQ");
+  
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
-app.UseCors();
 
+app.UseCors();
 app.ConfigureSignalREndpoints();
 app.MapControllers();
+
+app.MapPost("/chat", async (IChatCompletionService chatCompletionService, ChatModel chatModel) =>
+{
+    var response = await chatCompletionService.GetChatMessageContentAsync(chatModel.Input);
+    return response?.ToString() ?? "Boş yanıt alındı!";
+}).WithRequestTimeout(TimeSpan.FromMinutes(10));
 
 app.Run();
